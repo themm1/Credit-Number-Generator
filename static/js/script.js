8df2f88e15d8eb429d29d0352719e1eeecff412c
themm1 <madzin.m@gmail.com>
2021-02-11 11:33:45 +0100

    with javascrpt - no css

--- static/js/script.js
+++ static/js/script.js
@@ -1,19 +1,33 @@
 $(document).ready(function() {
 
 	$('#generate').on('submit', function(event) {
-
 		$.ajax({
-            data : {
-                brand : $('#brand').val()
-            },
+			data : {
+				brand : $('#brand').val()
+			},
 			type : 'POST',
 			url : '/generate'
 		})
-        .done(function(data) {
-			$('#generatedNumber').text(data.number).show();
+
+		.done(function(data) {
+			$('#generatedNumber').text(data.number);
 		});
 		event.preventDefault();
-
 	});
 
+
+	$('#validate').on('submit', function(event) {
+		$.ajax({
+			data : {
+				number : $('#number').val()
+			},
+			type : 'POST',
+			url : '/validate'
+		})
+
+		.done(function(data) {
+			$('#validatedNumber').text(data.message);
+		});
+		event.preventDefault();
+	});
 });
